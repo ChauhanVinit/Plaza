58742ec1baad534a670d83c963420a155052f836
ChauhanVinit <vinit.c@teamswork.in>
2025-06-27 19:28:10 +0530

    updated

--- utils/Input.jsx
+++ utils/Input.jsx
@@ -9,7 +9,7 @@ const Input = ({type,placeholder,value,name,id,onChange,className}) => {
       value={value}
       name={name}
       onChange={onChange}
-      className={` w-full border h-[50px] border-[#7198FE]/50 rounded bg-white px-5  outline-none ring-0 placeholder:text-[#45535E] font-dmSans text-base font-medium text-[#171A1D]  ${className} `}
+      className={` w-full border h-[50px] border-[#7198FE]/50 rounded bg-white px-5  outline-none ring-0 placeholder:text-[#45535E] font-dmSans tracking-[-0.5px] text-base font-medium text-[#171A1D]  ${className} `}
     />
   );
 };
--- utils/Textarea.jsx
+++ utils/Textarea.jsx
@@ -7,7 +7,7 @@ const Textarea = ({ onChange, placeholder, name,className }) => {
     onChange={onChange}
     placeholder={placeholder}
     className={`!min-h-[90px] w-full border border-[#7198FE]/50 rounded bg-white
-       py-[17px] px-5 outline-none ring-0 placeholder:text-[#45535E] font-dmSans text-base font-medium text-[#171A1D] ${className} `}
+       py-[17px] px-5 outline-none ring-0 placeholder:text-[#45535E] font-dmSans tracking-[-0.5px] text-base font-medium text-[#171A1D] ${className} `}
   />
   )
 }
